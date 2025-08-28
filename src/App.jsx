6d6119bcd5b0e27daffc009ed5fbcf2324dc93ac
Maximilian <scientificresearch309@gmail.com>
2025-08-28 21:06:24 +0200

    if else statement in react => with &&

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import './App.css';
 
 const title = 'Our JSX Bascics Lessons';
+const userIsOnline = true;
 
 export const App = () => {
   return (
     <div className="App">
       <h1>Welcome to React-Vite-Javascript</h1>
+      {userIsOnline && <p>user is online</p>}
+      <hr />
       <p>{title}</p>
+      <hr />
     </div>
   );
 };
