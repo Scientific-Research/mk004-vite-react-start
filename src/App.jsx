4977b9b1cfae8aae27076315ff401c7abf590c10
Maximilian <scientificresearch309@gmail.com>
2025-08-28 20:26:31 +0200

    display variable in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 // import './App.css';
 
+const title = 'Our JSX Bascics Lessons';
+
 export const App = () => {
   return (
     <div className="App">
       <h1>Welcome to React-Vite-Javascript</h1>
+      <p>{title}</p>
     </div>
   );
 };
